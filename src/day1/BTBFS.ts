b7460c403ba4f0733fb36f83a71462beb712e27f
MohammadHossein Jahanbakhsh <m.jahanbakhsh@bluteam.ir>
2024-02-11 20:36:54 +0330

    day 2 part1 / finish breadth first search(which is only one algorithm)

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,26 @@
+//needle is the number we're looking for.
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
+
+    while(q.length){
+        const curr  = q.shift();
+        if(!curr) continue
+
+        
+        //search
+        if(curr?.value===needle){
+            return true
+        }   
+
+        if(curr?.left){
+            q.push(curr.left)
+        }
+        if(curr?.right){
+            q.push(curr.right)
+        }
+
+    }
+    return false
 
 }

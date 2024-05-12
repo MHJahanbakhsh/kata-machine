b7dea8c1fc57805a916cf6eddd3e2e95fdf44932
MohammadHossein Jahanbakhsh <mammadjahanbakhsh@yahoo.com>
2024-05-12 21:31:02 +0330

    REFINEMENTS

--- src/day1/MergeSort.ts
+++ src/day1/MergeSort.ts
@@ -1,22 +1,23 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function mergeSort(array) {
+function mergeSort(array: number[]): number[] {
     if (array.length === 1) {
         return array;
     }
 
-    // Split Array in into right and left
+    // Split Array into right and left
     const length = array.length;
     const middle = Math.floor(length / 2);
     const left = array.slice(0, middle);
     const right = array.slice(middle);
 
-
     return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right) {
-    const result = [];
+//left and right are already sorted on their own. 
+// now we are going to merge them together while maintaining its sorting attribute sorted way
+function merge(left: number[], right: number[]): number[] {
+    const result: number[] = [];
     let leftIndex = 0;
     let rightIndex = 0;
 
@@ -29,8 +30,10 @@ function merge(left, right) {
             rightIndex++;
         }
     }
-    console.log(result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex)))
-
+    // we are merging two arrays.let say [1,3,5] & [2,6,12]
+    //after while loop breaks, we have this for result: [1,2,3,5]  leftIndex:2 rightIndex:1
+    //since we we don't know what items of which array are left;
+    //we use code below to kinda concat the remaining items without actually having duplicates in result
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
